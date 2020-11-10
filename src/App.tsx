5bea1f7033e5ffa78d0f3e83dec367ef695ff97e
erlendoeien <pappegoyen1@gmail.com>
2020-11-11 00:23:11 +0100

    Remove debug

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import MainRouterSwitch from './components/routing/MainRouterSwitch';
 
 const App: React.FC = () => {
-    console.log(process.env.REACT_APP_API_ENDPOINT);
     return (
         <Router>
             <div className="App">
